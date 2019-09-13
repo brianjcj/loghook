a5b88e199f7764c14c0cd6713df471b8ae1a7a0c
jiangchengjun <jiangchengjun@yy.com>
2019-09-13 18:40:59 +0800

    remove dist dir

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { LogHook, getLogger, log, PluginHookFnType, LogLevel, setLogLevel } from "./logHook";
-import { Hook_LogView, setLogVisible, setRecordLog } from "./logView";
-export { LogHook, getLogger, log, PluginHookFnType, LogLevel, setLogLevel };
-export { Hook_LogView, setLogVisible, setRecordLog };
